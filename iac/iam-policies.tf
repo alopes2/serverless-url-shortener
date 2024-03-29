ec31907e553d0d0cf1b8733cb83b2b230c4cd247
Andre <andrevitorlopes@gmail.com>
2024-03-29 14:04:08 +0100

    Fix policy name

--- iac/iam-policies.tf
+++ iac/iam-policies.tf
@@ -12,7 +12,7 @@ data "aws_iam_policy_document" "create_short_url_lambda" {
   }
 }
 
-data "aws_iam_policy_document" "create_short_url_lambda" {
+data "aws_iam_policy_document" "allow_get_url_lambda" {
   statement {
     effect = "Allow"
 
