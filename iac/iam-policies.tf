9fde44491ff9d1b60edbecd32c96083dc2bb3060
Andre <andrevitorlopes@gmail.com>
2024-03-29 13:51:12 +0100

    Add redirect lambda

--- iac/iam-policies.tf
+++ iac/iam-policies.tf
@@ -11,3 +11,17 @@ data "aws_iam_policy_document" "create_short_url_lambda" {
     ]
   }
 }
+
+data "aws_iam_policy_document" "create_short_url_lambda" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "dynamodb:GetItem",
+    ]
+
+    resources = [
+      aws_dynamodb_table.urls.arn
+    ]
+  }
+}
--- iac/lambdas.tf
+++ iac/lambdas.tf
@@ -6,3 +6,12 @@ module "create_short_url_lambda" {
     data.aws_iam_policy_document.create_short_url_lambda.json
   ]
 }
+
+module "redirect_lambda" {
+  source           = "./modules/lambda"
+  name             = "redirect"
+  source_file_path = "./init_code/index.mjs"
+  policies = [
+    data.aws_iam_policy_document.create_short_url_lambda.json
+  ]
+}
